c09b9989e60e6e67f35b9273f1b031057164ff91
Manish <25amticss330@gmail.com>
2026-04-18 18:45:28 +0530

    deleted src folder

--- src/routes/itinerary.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-  res.json({ message: 'itinerary route works' });
-});
-
-export default router;
--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-  res.json({ message: 'user route works' });
-});
-
-export default router;
